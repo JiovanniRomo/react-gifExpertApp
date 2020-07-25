67248bb3a13a94adb8c3ee88cadceb380851e863
Jiovanni Romo <srr190802@gmail.com>
2020-07-24 19:32:19 -0500

    docs has been added

---
+++ docs/precache-manifest.0c8da548e54632e70dbe9886f13e7626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d73ad93355e5f7a35da627913506f5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0a174cb4909e48a641b",
+    "url": "/static/css/main.de8c248a.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a174cb4909e48a641b",
+    "url": "/static/js/main.ca123fac.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
